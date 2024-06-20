634999bf20efd1a9ca3ecf3abdaa5a56262da651
SId02 <31335947+SId02@users.noreply.github.com>
2024-06-20 15:45:00 +0530

    add

---
+++ src/views/CurrencyConverter.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="flex justify-center h-screen">
+      <div class="container max-w-md p-5 mx-auto">
+        <h1 class="mb-4 text-3xl font-bold">Currency Converter</h1>
+        
+      </div>
+    </section>
+  </template>
+  
+  <script>
+ 
+  </script>
+  
+  <style scoped>
+  /* Add your custom styles here */
+  </style>
---
+++ src/views/PasswordGenerator.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+      <div class="container p-5 mt-5 text-center">
+        <h1 class="title">Password Generator</h1>
+      </div>
+  
+      
+  
+      
+    </section>
+  </template>
+  
+  <script>
+ 
+  </script>
