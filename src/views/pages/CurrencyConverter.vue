d8ea0f3ed97531b2716a567be518f5fd93e2ea5e
SId02 <31335947+SId02@users.noreply.github.com>
2024-07-15 12:19:37 +0530

    add

--- src/views/pages/CurrencyConverter.vue
+++ src/views/pages/CurrencyConverter.vue
@@ -68,5 +68,5 @@
   </script>
   
   <style scoped>
-  /* Add your custom styles here */
+  
   </style>
---
+++ src/views/pages/FormValidation.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3>Form Validation</h3>
+</template>
+  
+<script>
+
+
+</script>
